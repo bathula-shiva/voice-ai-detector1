2dd2abcff509dd6796424eae9551044d5af7cce9
bathula-shiva <bathulashiva1845@gmail.com>
2026-01-31 22:47:08 +0530

    Update main.py

--- voice_ai_detector/app/main.py
+++ voice_ai_detector/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from app.audio_utils import extract_features
-from app.model import predict_voice
+from app.model import predict
 
 app = FastAPI(title="Voice AI Detector")
 
@@ -8,13 +8,9 @@ app = FastAPI(title="Voice AI Detector")
 async def detect_voice(file: UploadFile = File(...)):
     audio_bytes = await file.read()
     features = extract_features(audio_bytes)
-    prediction = predict_voice(features)
+    prediction = predict(features)
 
     return {
         "prediction": prediction["result"],
         "confidence": prediction["confidence"]
     }
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
